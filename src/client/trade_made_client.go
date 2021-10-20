51538ddce0e20e379cf1dab41250e6071c4f800d
Jose Montero <jalfmontero@Falabella.cl>
2021-10-20 12:45:13 -0300

    add secrets to store api_key

--- src/client/trade_made_client.go
+++ src/client/trade_made_client.go
@@ -5,28 +5,41 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
+	"os"
+	"strings"
 	"superheroe-api/superheroe-golang-api/src/entity"
 
 	log "github.com/sirupsen/logrus"
 )
 
-type tradeMade struct{}
-
-var (
-	currencies = "EURUSD,GBPUSD"
-	api_key    = "7N5lZrYSYrdbDwlP85iT"
-	url        = "https://marketdata.tradermade.com/api/v1/live?currency=" + currencies + "&api_key=" + api_key
-)
+type tradeMadeStruct struct {
+	currencies string
+	api_key    string
+	url        string
+}
 
 //NewTradeMade initialice a new trade made controller
 func NewTradeMade() Client {
+	api_key := os.Getenv("API_KEY")
+	currencies := os.Getenv("CURRENCIES")
+	if len(strings.TrimSpace(currencies)) == 0 {
+		currencies = "EURUSD,GBPUSD"
+	}
+	if len(strings.TrimSpace(api_key)) == 0 {
+		api_key = "12345"
+	}
+
 	log.SetFormatter(&log.JSONFormatter{})
-	return &tradeMade{}
+	return &tradeMadeStruct{
+		currencies: currencies,
+		api_key:    api_key,
+		url:        "https://marketdata.tradermade.com/api/v1/live?currency=" + currencies + "&api_key=" + api_key,
+	}
 }
 
 //Get makes an http get request to the public TradeMade API
-func (c *tradeMade) Get() (interface{}, error) {
-	res, err := http.Get(url)
+func (c *tradeMadeStruct) Get() (interface{}, error) {
+	res, err := http.Get(c.url)
 	if err != nil {
 		log.WithFields(log.Fields{"package": "client", "client": "TradeMade", "method": "Get"}).Error(err.Error())
 		return nil, err
